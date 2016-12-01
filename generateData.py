69ef9dae777804c8ddf0786b8330e1ceababe359
Shriyaa Mittal <shriyaamittal@users.noreply.github.com>
2016-12-01 15:00:48 -0600

    Update generateData.py

--- generateData.py
+++ generateData.py
@@ -4,7 +4,7 @@ import time
 import os
 
 ## Constants
-nuc = ['A','G','T','C']
+nuc = ['A','C','G','T']
 freq = [0.25,0.25,0.25,0.25]
 filename='uniform_bg_ICPC_to_p.txt'
 sequence_filename='sequences.fa'
